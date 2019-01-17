3130aa9c1d3aae12f04284a95a6da5e72b063cf9
Evgeniy <evg.ifmoitmo@gmail.com>
2019-01-17 12:00:38 +0400

    Timer ready

--- Form1.cs
+++ Form1.cs
@@ -20,9 +20,6 @@ namespace Timer
         string time_total;
         DateTime date = new DateTime();
         DateTime date_total = new DateTime();
-        FileStream file = new FileStream("time_tracker.conf", FileMode.OpenOrCreate);
-        StreamWriter WriteFile = new StreamWriter("log.txt");
-
 
         public Form1()
         {
@@ -38,7 +35,7 @@ namespace Timer
             i++;
 
             date = date.AddSeconds(1);
-            date_total = date.AddSeconds(1);
+            date_total = date_total.AddSeconds(1);
 
             int hh = date.Hour;
             int mm = date.Minute;
@@ -59,13 +56,17 @@ namespace Timer
             {
                 time += "0" + hh;
             }
-            else if (hht < 10)
+            else
+            {
+                time += hh;
+            }
+
+            if (hht < 10)
             {
                 time_t += "0" + hht;
             }
             else
             {
-                time += hh;
                 time_t += hht;
             }
             time += ":";
@@ -76,13 +77,17 @@ namespace Timer
             {
                 time += "0" + mm;
             }
-            else if (mmt < 10)
+            else
+            {
+                time += mm;
+            }
+
+            if (mmt < 10)
             {
                 time_t += "0" + mmt;
             }
             else
             {
-                time += mm;
                 time_t += mmt;
             }
             time += ":";
@@ -93,13 +98,17 @@ namespace Timer
             {
                 time += "0" + ss;
             }
-            else if (sst < 10)
+            else
+            {
+                time += ss;
+            }
+
+            if (sst < 10)
             {
                 time_t += "0" + sst;
             }
             else
             {
-                time += ss;
                 time_t += sst;
             }
 
@@ -111,90 +120,43 @@ namespace Timer
         {
             StreamReader ReadFile = File.OpenText("time_tracker.cfg");
             string[] lines = ReadFile.ReadToEnd().Split('\n');
+            ReadFile.Close();
+            date_total = DateTime.MinValue;
+
             time_total = lines[0];
             label_total.Text = lines[0];
             label_last.Text = lines[1];
+
+            TimeSpan ts = new TimeSpan();
+            ts = TimeSpan.Parse(time_total);
+            date_total = date_total.Date + ts;
+
         }
 
         private void WriteInfoToFile()
         {
-            //byte[] array = System.Text.Encoding.Default.GetBytes(label_total.Text);
-            //file.Write(array, 0, array.Length);
+            StreamWriter WriteFile = new StreamWriter("time_tracker.cfg");
+            WriteFile.WriteLine(label_total.Text);
+            WriteFile.WriteLine(label_current.Text);
+            WriteFile.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ReadInfoFromFile();
             date = DateTime.MinValue;
-            date_total = DateTime.MinValue;
-            date_total = date_total.AddHours(int.Parse(time_total.Substring(0, 2)));
-            date_total = date_total.AddMinutes(int.Parse(time_total.Substring(2, 2)));
-            date_total = date_total.AddSeconds(int.Parse(time_total.Substring(4, 2)));
-            //label_total.Text = date_total.Hour.ToString();
-
             timer1.Enabled = true;
-        }
-
-        //Удалить
-        private void timer_tick(object sender, EventArgs e)
-        {
-            int hh = 0, mm = 0, ss = 0;
-            if(start)
-            {
-                date = DateTime.MinValue;
-                hh = date.Hour;
-                mm = date.Minute;
-                ss = date.Second;
-                //hh = DateTime.Now.Hour;
-                //mm = DateTime.Now.Minute;
-                //ss = DateTime.Now.Second;
-            }
-
-            string time = "";
-            if (hh < 10)
-            {
-                time += "0" + hh;
-            }
-            else
-            {
-                time += hh;
-            }
-            time += ":";
-
-            if (mm < 10)
-            {
-                time += "0" + mm;
-            }
-            else
-            {
-                time += mm;
-            }
-            time += ":";
-
-            if (ss < 10)
-            {
-                time += "0" + ss;
-            }
-            else
-            {
-                time += ss;
-            }
-
-            label_current.Text = time;
         }
 
         private void button_start_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
-            //ReadInfoFromFile();
-            //timer.Interval = 1000;
-            //timer.Tick += new EventHandler(this.timer_tick);
-            //timer.Start();
         }
 
         private void button_stop_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
+            WriteInfoToFile();
         }
     }
 }
